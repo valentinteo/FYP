f38ed69fbc102cac06bb1176d0482d8732449d54
valentinteo <23010715@myrp.edu.sg>
2025-06-03 19:42:50 +0800

    removal of node.js

---
+++ backend/src/app.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { connect_db, sqlServerDb } from './database/connection';
+import charityRoutes from './routes/charity.routes';
+
+dotenv.config();
+
+const app = express();
+
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+
+app.use(cors());
+
+app.use(express.json());
+app.use('/api/charities', charityRoutes);
+
+connect_db(); // connect to SQL Server
+// optional: sqlServerDb.sync() – only if needed again here
+
+console.log({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
