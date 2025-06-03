bdd74c2f11513cd65dccc68ef9586c6b054671b1
valentinteo <23010715@myrp.edu.sg>
2025-06-03 22:39:12 +0800

    nil

--- backend1/src/middleware/upload.ts
+++
@@ -1,8 +0,0 @@
-import multer from 'multer';
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, 'uploads/'),
-  filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname),
-});
-
-export const uploadMiddleware = multer({ storage }).single('charity_image');
